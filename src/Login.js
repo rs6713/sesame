c78bcb5e1bcee4b5b44005f6ed930a0e31fc79f7
Gary Roberts <tallstreet@gmail.com>
2018-05-26 18:25:04 +0100

    Add login page

---
+++ src/Login.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div class="input-group_login">
+          <input
+            type="text"
+            class="form-control login"
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+          />
+        </div>
+        <div class="input-group_login">
+          <input
+            type="text"
+            class="form-control login"
+            placeholder="Password"
+            aria-label="Password"
+            aria-describedby="basic-addon1"
+          />
+        </div>
+
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <button type="button" class="btn_login btn-secondary">
+              Login
+            </button>
+          </li>
+          <li class="nav-item">
+            <button type="button" class="btn_login btn-secondary">
+              Signup
+            </button>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Login from "./Login";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+if (window.location.hash === "#login") {
+  ReactDOM.render(<Login />, document.getElementById("root"));
+} else {
+  ReactDOM.render(<App />, document.getElementById("root"));
+}
 registerServiceWorker();
