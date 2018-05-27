ea6b5c53e3d06e9c4726c22f512f641d9f66a402
Gary Roberts <tallstreet@gmail.com>
2018-05-27 12:36:36 +0100

    Add homepage

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div className="App">Content goes here</div>;
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Home from "./Home";
 import Login from "./Login";
 import Signup from "./Signup";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Tab, Tabs } from "@blueprintjs/core";
 
 ReactDOM.render(
   <Router>
     <div>
-      <Route exact={true} path="/" component={App} />
+      <div className="center_block">
+        <img className="centered" src="/logo.PNG" alt="" />
+      </div>
+      <Route exact={true} path="/" component={Home} />
+      <Route exact={true} path="/insurance" component={App} />
       <Route path="/login" component={Login} />
       <Route path="/signup" component={Signup} />
+      <div className="nav">
+        <Tabs id="TabsExample" selectedTabId="rx">
+          <li className="pt-tab" role="tab">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="pt-tab" role="tab">
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li className="pt-tab" role="tab">
+            <Link to="/login">Login</Link>
+          </li>
+        </Tabs>
+      </div>
     </div>
   </Router>,
   document.getElementById("root")
